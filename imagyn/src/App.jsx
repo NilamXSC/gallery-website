4ec4bd41205a66b7eb1f8667575f8f258aecb041
Nilam Chakraborty <160915859+NilamXSC@users.noreply.github.com>
2026-03-09 22:28:11 +0530

    Update App.jsx

--- imagyn/src/App.jsx
+++ imagyn/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Hero from "./components/Hero";
 import GalleryButtons from "./components/GalleryButtons";
-import Lightbox from "./components/Lightbox"; // optional if you’re keeping the zoom feature
+import Lightbox from "./components/Lightbox"; // optional if you’re keeping the zoom featuredsd
 
 export default function App() {
   return (
